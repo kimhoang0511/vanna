825d1f0a6c46e38b37df1f855e034f5967b56e14
kimhoang0511 <kimhoang05111@gmail.com>
2025-10-27 21:05:43 +0700

    Fix MRO: Put VietnameseVanna first in inheritance order

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ class VannaService:
         from vanna.chromadb import ChromaDB_VectorStore
         from vanna.openai import OpenAI_Chat
         
-        class MyVanna(ChromaDB_VectorStore, OpenAI_Chat, VietnameseVanna):
+        class MyVanna(VietnameseVanna, ChromaDB_VectorStore, OpenAI_Chat):
             def __init__(self, config=None):
                 ChromaDB_VectorStore.__init__(self, config=config)
                 OpenAI_Chat.__init__(self, config=config)
